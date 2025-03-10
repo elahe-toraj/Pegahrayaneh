023971b51b4bc22d0877cafbc6d5270d2792f302
elahe toraj | الهه ترج <torajelahe@gmail.com>
2025-03-11 00:40:46 +0330

    edit slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,68 +2,89 @@ import React, { useState } from "react";
 import SliderDetails from "./SliderDetails";
 
 const Slider = () => {
-    const slides = [
-        {
-            img: "/img/1.png",
-            title: "کیس گیم",
-            type: "گیگیلی",
-            description: "یک کیس خوب و عالی برای بازی چه ویژگی هایی دارد ؟"
-        },
-        {
-            img: "/img/2.png",
-            title: "کیس گیم",
-            type: "گیگیلی",
-            description: "یک کیس خوب و عالی برای بازی چه ویژگی هایی دارد ؟"
-        },
-        {
-            img: "/img/3.png",
-            title: "کیس گیم",
-            type: "گیگیلی",
-            description: "یک کیس خوب و عالی برای بازی چه ویژگی هایی دارد ؟"
-        }
-    ];
+  const slides = [
+    {
+      img: "/img/1.png",
+      title: "کیس گیم",
+      type: "گیم",
+      description: "یک کیس خوب و عالی برای بازی چه ویژگی هایی دارد؟",
+    },
+    {
+      img: "/img/2.png",
+      title: "کیس گیم",
+      type: "گیم",
+      description: "یک کیس خوب و عالی برای بازی چه ویژگی هایی دارد؟",
+    },
+    {
+      img: "/img/3.png",
+      title: "کیس گیم",
+      type: "گیم",
+      description: "یک کیس خوب و عالی برای بازی چه ویژگی هایی دارد؟",
+    },
+  ];
 
-    const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    const nextSlide = () => {
-        setActiveIndex((prev) => (prev + 1) % slides.length);
-    };
+  const nextSlide = () => {
+    setActiveIndex((prev) => (prev + 1) % slides.length);
+  };
 
-    const prevSlide = () => {
-        setActiveIndex((prev) => (prev - 1 + slides.length) % slides.length);
-    };
+  const prevSlide = () => {
+    setActiveIndex((prev) => (prev - 1 + slides.length) % slides.length);
+  };
 
-    return (
-        <section>
-            <div className="slider">
-                <div className="list">
-                    {slides.map((item, index) => (
-                        <div key={index} className={`item ${index === activeIndex ? "active" : ""}`}>
-                            <img src={item.img} alt={`اسلاید ${index + 1}`} />
-                            <div className="content">
-                                <div className="title">{item.title}</div>
-                                <div className="type">{item.type}</div>
-                                <div className="description">{item.description}</div>
-                                <div className="button">
-                                    <button>مشاهده</button>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  const handleThumbnailClick = (index) => {
+    setActiveIndex(index);
+  };
 
-                <div className="thumbnail">
-                    {slides.map((item, index) => (
-                        <div key={index} className={`item ${index === activeIndex ? "active" : ""}`}>
-                            <img src={item.img} alt={`اسلاید ${index + 1}`} />
-                        </div>
-                    ))}
-                </div>
-
-                <SliderDetails onNext={nextSlide} onPrev={prevSlide} />
+  return (
+    <section>
+      <div
+        className="slider"
+        style={{
+          backgroundImage: `url(${slides[activeIndex].img})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          transition: "background-image 0.5s ease-in-out"
+        }}
+      >
+        <div className="list">
+          <div className="item active">
+            <img src={slides[activeIndex].img} alt={`اسلاید ${activeIndex + 1}`} />
+            <div className="content">
+              <div className="title">{slides[activeIndex].title}</div>
+              <div className="type">{slides[activeIndex].type}</div>
+              <div className="description">{slides[activeIndex].description}</div>
+              <div className="button">
+                <button>مشاهده</button>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+
+        <div className="thumbnail">
+          {slides
+            .slice()
+            .reverse()
+            .map((item, index) => {
+              const reversedIndex = slides.length - 1 - index; 
+              return (
+                <div
+                  key={index}
+                  className={`item ${reversedIndex === activeIndex ? "active" : ""}`}
+                >
+                  <img src={item.img} alt={`اسلاید ${index + 1}`} />
+                </div>
+              );
+            })}
+        </div>
+
+
+
+        <SliderDetails onNext={nextSlide} onPrev={prevSlide} />
+      </div>
+    </section>
+  );
 };
 
 export default Slider;
